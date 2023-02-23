173c7935e14ccb56bcc8769be3dd927d132d9180
Malcolm Wanless <32682177+mjwanless@users.noreply.github.com>
2023-02-22 21:04:16 -0800

    Segment movement basics (video 185 finished)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 # Setup of screen
 screen = Screen()
@@ -6,22 +7,33 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game!")
 
+# segment list for building the snake out
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
+for position in starting_positions:
+	new_segment = Turtle(shape="square")
+	new_segment.color("white")
+	new_segment.pu()
+	new_segment.goto(position)
+	segments.append(new_segment)
+
 # Running code
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(x=-20, y=0)
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(x=-40, y=0)
+game_is_on = True
 
+while game_is_on:
+	screen.update()
+	time.sleep(0.1)
 
+	for seg_num in range((len(segments)-1), 0, -1):
+		new_x = segments[(seg_num - 1)].xcor()
+		new_y = segments[(seg_num - 1)].ycor()
+		segments[seg_num].goto(new_x, new_y)
 
-
-
-
+	segments[0].forward(20)
 
 # Needed for screen close out at end
+
 screen.exitonclick()

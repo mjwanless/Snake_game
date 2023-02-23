21a1471de24dc8ce4507f7da180390e0d3dd4f72
Malcolm Wanless <32682177+mjwanless@users.noreply.github.com>
2023-02-22 20:14:05 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Screen, Turtle
+
+# Setup of screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game!")
+
+
+
+
+
+
+
+# Needed for screen close out at end
+screen.exitonclick()

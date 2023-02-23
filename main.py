4e69cb39498af942e24039ee999a338fb561f1ee
Malcolm Wanless <32682177+mjwanless@users.noreply.github.com>
2023-02-22 20:39:54 -0800

    Segments generated (runs fine)

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game!")
 
+# Running code
+
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+segment_2.goto(x=-20, y=0)
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+segment_3.goto(x=-40, y=0)
+
 
 
 
